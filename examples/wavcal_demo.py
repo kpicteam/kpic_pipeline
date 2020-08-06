fc7f2a60e2f2fbfcfdcd6ad8966bd6f78b2b037f
jruffio <jruffio@caltech.edu>
2020-08-05 20:18:20 -0700

    first version cleaned wavecal function and tutorial.

---
+++ examples/wavcal_demo.py
@@ -0,0 +1,147 @@
+import os
+import multiprocessing as mp
+from glob import glob
+import astropy.io.fits as fits
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import RegularGridInterpolator
+from kpicdrp.wavecal import *
+
+def plot_kpic_spectrum(arr,wvs=None,arr_err=None,ax_list=None,linestyle="-",linewidth=2,color=None,label=None):
+    if ax_list is None:
+        _ax_list = []
+
+    if wvs is None:
+        wvs = np.arange(arr.shape[1])
+
+    for order_id in range(arr.shape[0]):
+        if ax_list is None:
+            plt.subplot(arr.shape[0], 1, arr.shape[0]-order_id)
+            _ax_list.append(plt.gca())
+        else:
+            plt.sca(ax_list[order_id])
+        plt.plot(wvs[order_id,:],arr[order_id,:],linestyle=linestyle,linewidth=linewidth,label=label,color=color)
+        if arr_err is not None:
+            plt.fill_between(wvs[order_id,:],
+                             arr[order_id,:] - arr_err[order_id,:],
+                             arr[order_id,:] + arr_err[order_id,:],
+                             label=label+" (err)", alpha=0.5,color=color)
+
+    if ax_list is None:
+        return _ax_list
+    else:
+        return ax_list
+
+
+
+if __name__ == "__main__":
+
+    try:
+        import mkl
+
+        mkl.set_num_threads(1)
+    except:
+        pass
+
+    numthreads = 10
+    mypool = mp.Pool(processes=numthreads)
+    # mypool = None
+
+    ## Change local directory
+    kpicpublicdir = "/scr3/jruffio/data/kpic/public_kpic_data/"
+
+    ## Path relative to the public kpic directory
+    filelist_spectra = glob(os.path.join(kpicpublicdir, "20200702_HIP_81497", "*fluxes.fits"))
+    filename_oldwvs = os.path.join(kpicpublicdir, "utils", "first_guess_wvs_20200607_HIP_81497.fits")
+    filename_phoenix_rvstandard = os.path.join(kpicpublicdir, "utils", "HIP_81497_lte03600-1.00-0.0.PHOENIX-ACES-AGSS-COND-2011-HiRes.fits")
+    filename_phoenix_wvs =os.path.join(kpicpublicdir, "utils", "WAVE_PHOENIX-ACES-AGSS-COND-2011.fits")
+    filename_line_width = glob(os.path.join(kpicpublicdir, "20200702_HIP_81497", "calib", "*_line_width_smooth.fits"))[0]
+    filelist_atran = glob(os.path.join(kpicpublicdir, "utils","atran","atran_13599_30_*.dat"))
+
+    out_filename = os.path.join(kpicpublicdir, "20200702_HIP_81497", "calib", "20200702_HIP_81497_wvs.fits")
+
+    target_rv = -55.567 #km/s
+    N_nodes_wvs=6 # Number of spline nodes for the wavelength solution
+    blaze_chunks=5 # Number of chunks of the "blaze profile" (modeled as a piecewise linear function)
+    init_grid_search = True # do a rough grid search of the wavcal before running optimizer
+    init_grid_dwv = 1e-4#3e-4 #mircrons, how far to go for the grid search. Caution: It can take quite a while!
+
+    # Read an old wavelength array to be used as the first guess
+    hdulist = fits.open(filename_oldwvs)
+    old_wvs = hdulist[0].data
+
+    hdulist = fits.open(filename_line_width)
+    line_width = hdulist[0].data
+    line_width_func_list = linewidth2func(line_width,old_wvs)
+
+    # Read the Phoenix model and wavelength corresponding to the RV standard star.
+    with fits.open(filename_phoenix_wvs) as hdulist:
+        phoenix_wvs = hdulist[0].data / 1.e4
+    crop_phoenix = np.where((phoenix_wvs > 1.8 - (2.6 - 1.8) / 2) * (phoenix_wvs < 2.6 + (2.6 - 1.8) / 2))
+    phoenix_wvs = phoenix_wvs[crop_phoenix]
+    with fits.open(filename_phoenix_rvstandard) as hdulist:
+        phoenix_spec = hdulist[0].data[crop_phoenix]
+
+    # read spectra files
+    combined_spec,combined_err,avg_baryrv = stellar_spectra_from_files(filelist_spectra)
+    # plot_kpic_spectrum(combined_spec[1,:,:])
+    # plt.show()
+
+    new_wvs_arr = np.zeros(combined_spec.shape)
+
+    for fib in range(combined_spec.shape[0]):
+        if np.nansum(combined_spec[fib,:,:])==0:
+            continue
+        star_rv = target_rv-avg_baryrv[fib]
+
+        # broaden and create interpolation function for the Phoenix model
+        phoenix_line_widths = np.array(pd.DataFrame(line_width_func_list[fib](phoenix_wvs)).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[:, 0]
+        phoenix_conv = convolve_spectrum_line_width(phoenix_wvs, phoenix_spec, phoenix_line_widths, mypool=mypool)
+        ##
+        phoenix_func = interp1d(phoenix_wvs, phoenix_conv / np.nanmax(phoenix_conv), bounds_error=False,fill_value=np.nan)
+
+        ## Create a interpolation function for the tellurics model (including water and zenith angle)
+        atrangridname = os.path.join(kpicpublicdir, "utils","atran", "atran_grid_f{0}.fits".format(fib))
+        if 0:
+            # enable this section to broaden and save the atran grid of models.
+            # The resulting fits file can directly be used to generate a regular grid interpolator as shown below.
+            # the broadening is specific to the line width calibration, so it might vary from epoch to epoch and maybe
+            # even between fibers. That being said, it might not make a big difference...
+            save_atrangrid(filelist_atran, line_width_func_list[fib], atrangridname,mypool=mypool)
+            # exit()
+        hdulist = fits.open(atrangridname)
+        atran_grid =  hdulist[0].data
+        water_unique =  hdulist[1].data
+        angle_unique =  hdulist[2].data
+        atran_wvs =  hdulist[3].data
+        hdulist.close()
+        ##
+        atran_interpgrid = RegularGridInterpolator((water_unique,angle_unique),atran_grid,method="linear",bounds_error=False,fill_value=0.0)
+
+        # Derive wavecal for a single fiber
+        new_wvs_fib,model,out_paras = fit_wavecal_fib(old_wvs[fib,:,:],combined_spec[fib,:,:],combined_err[fib,:,:],
+                                                     phoenix_func,star_rv,atran_wvs,atran_interpgrid,
+                                                     N_nodes_wvs=N_nodes_wvs,
+                                                     blaze_chunks=blaze_chunks,
+                                                     init_grid_search = init_grid_search,
+                                                     init_grid_dwv = init_grid_dwv,
+                                                     mypool=mypool)
+        new_wvs_arr[fib,:,:] = new_wvs_fib
+
+        plt.figure(fib+1,figsize=(12,12))
+        ax_list = plot_kpic_spectrum(combined_spec[fib,:,:],wvs=new_wvs_fib,arr_err=combined_err[fib,:,:],color="blue",label="data")
+        ax_list = plot_kpic_spectrum(model,wvs=new_wvs_fib,color="orange",label="model",ax_list=ax_list)
+        plt.legend()
+        print("Saving " + out_filename.replace(".fits","_f{0}.png".format(fib)))
+        plt.savefig(out_filename.replace(".fits","_f{0}.png".format(fib)))
+
+    hdulist = pyfits.HDUList()
+    hdulist.append(pyfits.PrimaryHDU(data=new_wvs_arr,header=fits.open(filelist_spectra[0])[0].header))
+    print("Saving "+ out_filename)
+    try:
+        hdulist.writeto(out_filename, overwrite=True)
+    except TypeError:
+        hdulist.writeto(out_filename, clobber=True)
+    hdulist.close()
+
+    plt.show()
--- kpicdrp/wavecal.py
+++ kpicdrp/wavecal.py
@@ -1,147 +1,269 @@
 import numpy as np
-import scipy.ndimage as ndi
-import scipy.optimize
+from copy import copy
 import astropy.io.fits as fits
-import astropy.io.ascii as ascii
-from astropy.time import Time
-from astropy.coordinates import SkyCoord, EarthLocation
-import astropy.units as u
-import astropy.constants as consts
-import astropy.table as table
-import matplotlib.pylab as plt
-from astropy.modeling import models, fitting
+from scipy.interpolate import interp1d
+import itertools
+from scipy.optimize import minimize
+from scipy.interpolate import InterpolatedUnivariateSpline
+import pandas as pd
+from kpicdrp.utils import *
 
-from astropy.utils import iers
-from astropy.utils.iers import conf as iers_conf
-print(iers_conf.iers_auto_url)
-#default_iers = iers_conf.iers_auto_url
-#print(default_iers)
-iers_conf.iers_auto_url = 'https://datacenter.iers.org/data/9/finals2000A.all'
-iers_conf.iers_auto_url_mirror = 'ftp://cddis.gsfc.nasa.gov/pub/products/iers/finals2000A.all'
-iers.IERS_Auto.open()  # Note the URL
-
-
-def fit_to_model(fitparams, xs, data, redshift, tell_wv, tell_trans, template_wvs, template_flux, response_xs, response_flux):
-    
-    #a2 = fitparams[0]
-    #a1 = fitparams[1]
-    #a0 = fitparams[2]
-    polyparams = fitparams
-    params = [polyparams[0]/1e25, polyparams[1]/1e20, polyparams[2]/1e15, polyparams[3]/1e10, polyparams[4]/1e5, polyparams[5]]
-
-    xcoords = xs
-    #wvs_model = a2 * xcoords**2 + a1 * xcoords + a0
-    wvs_model = np.poly1d(params)(xcoords)
-    
-    #tell_trans = ndi.gaussian_filter(tell_trans, 3)
-
-    template_interp = np.interp(wvs_model, template_wvs*(1+redshift), template_flux)
-    tell_interp = np.interp(wvs_model, tell_wv, tell_trans)
-    response_interp = np.interp(xcoords, response_xs, response_flux)
-
-    model = template_interp *  response_interp  * tell_interp
-    #model *= np.percentile(data, 95)/np.percentile(model, 95)
-    model /= ndi.median_filter(model, 300)
-    data_filt = data / ndi.median_filter(data, 300)
-    
-    return model - data_filt
-    #return -np.nansum(tell_interp*data)
-    
-
-def update_wv_soln(order_fluxes, rel_v, old_wv_solns, tell_wvs, tell_model, star_wvs, star_model, resp_xs, resp_model, xs=None, plot=False):
-    new_wv_solns = []
-
-    beta = rel_v/consts.c.to(u.km/u.s).value
-    redshift = np.sqrt((1 + beta)/(1 - beta)) - 1
-
-    if xs is None:
-        xs = np.arange(10, 2000)
-
-    for order_index in range(9):
-
-        fit_data = np.copy(order_fluxes[order_index])
-        order_response = resp_model[order_index]
-        #fit_data /= np.nanpercentile(fit_data, 98)#ndi.median_filter(fit_data, 300)
-        #fit_data[np.where(np.abs(fit_data - np.median(fit_data)) > 10)] = np.median(fit_data)
-        good = np.where(~np.isnan(fit_data))
-        
-        polyparams = old_wv_solns[order_index]
-        polyparams = [0*1e25, polyparams[-5]*1e20, polyparams[-4]*1e15, polyparams[-3]*1e10, polyparams[-2]*1e5, polyparams[-1]]
-        fitparams = polyparams
-        
-    #     if order_index < 6:
-    #         tell_model = np.ones(tell_dat['wvs'].shape)
-    #     else:
-    #         tell_model = tell_dat['trans']
-        
-        result = scipy.optimize.leastsq(fit_to_model,  fitparams, 
-                                        args=(xs[good], fit_data[good], redshift, tell_wvs, tell_model, star_wvs, star_model, resp_xs, order_response))
-
-        #print(result)
-        polyparams = result[0][0:]
-        delta_beta = 0 #result[0][0]
-        redshift = np.sqrt((1 + beta + delta_beta)/(1 - beta - delta_beta)) - 1
-        params = [polyparams[0]/1e25, polyparams[1]/1e20, polyparams[2]/1e15, polyparams[3]/1e10, polyparams[4]/1e5, polyparams[5]]
-        #print(params, delta_beta * consts.c.to(u.km/u.s).value)
-        wvsoln = np.poly1d(params)
-        thiswvs = wvsoln(xs)
-        #print(thiswvs)
-
-
-        
-        new_wv_solns.append(params)
-
-        if plot:
-            wvs_model = wvsoln(xs)
-
-            template_interp = np.interp(wvs_model, star_wvs*(1+redshift), star_model)
-            tell_interp = np.interp(wvs_model, tell_wvs, tell_model)
-            response_interp = np.interp(xs, resp_xs, order_response)
-
-            model = template_interp * tell_interp * response_interp
-            model *= np.nanpercentile(fit_data, 95)/np.nanpercentile(model, 95)
-
-
-            plt.figure(figsize=(20,4))
-            plt.plot(thiswvs, fit_data - 0.25, 'b-')
-            plt.plot(wvs_model, model, 'k-', alpha=0.5)
-
-
-            plt.xlim(wvsoln([0,2048]))
-            #plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(0.001))
-            plt.grid()
-
-
-            plt.figure(figsize=(20,4))
-            plt.plot(thiswvs, ndi.gaussian_filter(fit_data,1), 'b-')
-            plt.plot(wvs_model, model, 'k-', alpha=0.5)
-
-
-            plt.xlim([wvs_model[50], wvs_model[500]])
-            #plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(0.001))
-            plt.grid()
-            plt.show()
-
-    return new_wv_solns
-
-keck = EarthLocation.from_geodetic(lat=19.8283*u.deg, lon=-155.4783*u.deg, height=4160*u.m)
-def compute_rel_vel(mjd, ra, dec, star_v):
+def edges2nans(spec):
     """
-    Computes the relative velocity of the star relative to Earth during the time of observation
+    Set edges to Nans. Hard coded.
+    """
+    cp_spec = copy(spec)
+    if np.size(spec.shape) == 3:
+        cp_spec[:,:,0:5] = np.nan
+        cp_spec[:,:,2000::] = np.nan
+    elif np.size(spec.shape) == 2:
+        cp_spec[:,0:5] = np.nan
+        cp_spec[:,2000::] = np.nan
+    elif np.size(spec.shape) == 1:
+        cp_spec[0:5] = np.nan
+        cp_spec[2000::] = np.nan
+    return cp_spec
 
+def save_atrangrid(filelist_atran,line_width_func,atrangridname,mypool=None):
+    """
+    Broaden and save a grid of telluric models using atran.
+    """
+    filelist_atran.sort() #'/scr3/jruffio/data/kpic/models/atran/atran_13599_30_500_2_0_1.9_2.6.dat'
+    water_list = np.array([int(atran_filename.split("_")[-5]) for atran_filename in filelist_atran])
+    angle_list = np.array([float(atran_filename.split("_")[-3]) for atran_filename in filelist_atran])
+    water_unique = np.unique(water_list)
+    angle_unique = np.unique(angle_list)
+
+    atran_spec_list = []
+    for water in water_unique:
+        for angle in angle_unique:
+            print(water,angle)
+            atran_filename = filelist_atran[np.where((water==water_list)*(angle==angle_list))[0][0]]
+            atran_arr = np.loadtxt(atran_filename).T
+            atran_wvs = atran_arr[1,:]
+            atran_spec = atran_arr[2,:]
+            atran_line_widths = np.array(pd.DataFrame(line_width_func(atran_wvs)).interpolate(method="linear").fillna(method="bfill").fillna(method="ffill"))[:, 0]
+            atran_spec_conv = convolve_spectrum_line_width(atran_wvs,atran_spec,atran_line_widths,mypool=mypool)
+            atran_spec_list.append(atran_spec_conv)
+
+    atran_grid = np.zeros((np.size(water_unique),np.size(angle_unique),np.size(atran_wvs)))
+    for water_id,water in enumerate(water_unique):
+        for angle_id,angle in enumerate(angle_unique):
+            atran_grid[water_id,angle_id,:] = atran_spec_list[np.where((water_list==water)*(angle_list==angle))[0][0]]
+
+    hdulist = fits.HDUList()
+    hdulist.append(fits.PrimaryHDU(data=atran_grid))
+    hdulist.append(fits.ImageHDU(data=water_unique))
+    hdulist.append(fits.ImageHDU(data=angle_unique))
+    hdulist.append(fits.ImageHDU(data=atran_wvs))
+    try:
+        hdulist.writeto(atrangridname, overwrite=True)
+    except TypeError:
+        hdulist.writeto(atrangridname, clobber=True)
+    hdulist.close()
+
+def wavcal_model(paras, x,spectrum,spec_err, star_func,telluric_wvs,telluric_interpgrid,N_nodes_wvs,blaze_chunks,fitsrv,rv):
+    c_kms = 299792.458
+    wvs_coefs = paras[0:N_nodes_wvs]
+    sig = 1
+    if fitsrv:
+        star_rv = paras[N_nodes_wvs]
+        water = paras[N_nodes_wvs+1]
+        angle = paras[N_nodes_wvs+2]
+    else:
+        star_rv = rv
+        water = paras[N_nodes_wvs]
+        angle = paras[N_nodes_wvs+1]
+
+    tel_func = interp1d(telluric_wvs,telluric_interpgrid([water,angle])[0],bounds_error=False,fill_value=0)
+
+    if N_nodes_wvs <=3:
+        M2 = np.zeros((np.size(x),(N_nodes_wvs)))
+        x_knots = x[np.linspace(0,len(x)-1,N_nodes_wvs,endpoint=True).astype(np.int)]#np.array([wvs_stamp[wvid] for wvid in )
+        for polypower in range(N_nodes_wvs-1):
+            if polypower == 0:
+                where_chunk = np.where((x_knots[polypower]<=x)*(x<=x_knots[polypower+1]))
+            else:
+                where_chunk = np.where((x_knots[polypower]<x)*(x<=x_knots[polypower+1]))
+            M2[where_chunk[0],polypower] = 1-(x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+            M2[where_chunk[0],1+polypower] = (x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+        wvs = np.dot(M2,wvs_coefs)
+    else:
+        x_knots = x[np.linspace(0,len(x)-1,N_nodes_wvs,endpoint=True).astype(np.int)]#np.array([wvs_stamp[wvid] for wvid in )
+        spl = InterpolatedUnivariateSpline(x_knots,wvs_coefs,k=3,ext=0)
+        wvs= spl(x)
+
+    M = np.zeros((np.size(x),(blaze_chunks+1)))
+    tmp = star_func(wvs*(1-star_rv/c_kms))*tel_func(wvs)
+    M0_mn = np.nanmean(tmp)
+    tmp /= M0_mn
+    x_knots = x[np.linspace(0,len(x)-1,blaze_chunks+1,endpoint=True).astype(np.int)]
+    for polypower in range(blaze_chunks):
+        if polypower == 0:
+            where_chunk = np.where((x_knots[polypower]<=x)*(x<=x_knots[polypower+1]))
+        else:
+            where_chunk = np.where((x_knots[polypower]<x)*(x<=x_knots[polypower+1]))
+        M[where_chunk[0],polypower] = 1-(x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+        M[where_chunk[0],1+polypower] = (x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+    M = tmp[:,None]*M
+
+    if 0:
+        deg_off = 1
+        Moff = np.zeros((np.size(x),(deg_off+1)))
+        x_knots = x[np.linspace(0,len(x)-1,deg_off+1,endpoint=True).astype(np.int)]
+        for polypower in range(deg_off):
+            if polypower == 0:
+                where_chunk = np.where((x_knots[polypower]<=x)*(x<=x_knots[polypower+1]))
+            else:
+                where_chunk = np.where((x_knots[polypower]<x)*(x<=x_knots[polypower+1]))
+            Moff[where_chunk[0],polypower] = 1-(x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+            Moff[where_chunk[0],1+polypower] = (x[where_chunk]-x_knots[polypower])/(x_knots[polypower+1]-x_knots[polypower])
+        M = np.concatenate([M,Moff], axis = 1)
+
+    where_data_finite = np.where(np.isfinite(spectrum))
+    d = spectrum[where_data_finite]
+    d_err = spec_err[where_data_finite]* sig
+    M = M[where_data_finite[0],:]
+    try:
+        p,chi2,rank,s = np.linalg.lstsq(M/d_err[:,None],d/d_err,rcond=None)
+        m=np.zeros(spectrum.shape)+np.nan
+        m[where_data_finite] = np.dot(M,p)
+        return m
+    except:
+        return None
+
+def wavcal_nloglike(paras, x,spectrum,spec_err, star_func,telluric_wvs,telluric_interpgrid,N_nodes_wvs,blaze_chunks,fitsrv,rv):
+    m = wavcal_model(paras, x,spectrum,spec_err, star_func,telluric_wvs,telluric_interpgrid,N_nodes_wvs,blaze_chunks,fitsrv,rv)
+    if m is not None:
+        nloglike = np.nansum((spectrum - m) ** 2 / (spec_err) ** 2)
+        return 1 / 2. * nloglike
+    else:
+        return np.inf
+
+def _fit_wavecal(paras):
+    x,wvs0,spectrum,spec_err, star_func,telluric_wvs,telluric_interpgrid,N_nodes_wvs,blaze_chunks,fitsrv,rv,\
+        init_grid_search,init_grid_dwv = paras
+    dwv = 3*(wvs0[np.size(wvs0)//2]-wvs0[np.size(wvs0)//2-1])
+
+    ## pre optimization with grid search and smaller dimensional space
+    if init_grid_search:
+        tmp_deg_wvs = 4
+        N_dwv = init_grid_dwv//dwv
+        x_knots = x[np.linspace(0,len(x)-1,tmp_deg_wvs,endpoint=True).astype(np.int)]#np.array([wvs_stamp[wvid] for wvid in )
+        Npix = spectrum.shape[-1]
+
+        wvs_min = np.arange(wvs0[0]-N_dwv*dwv,wvs0[0]+N_dwv*dwv,dwv)#-130*dwv/2
+        wvs_mid = np.arange(wvs0[Npix//3]-N_dwv*dwv,wvs0[Npix//3]+N_dwv*dwv,dwv)#-130*dwv/2
+        wvs_mid2 = np.arange(wvs0[2*Npix//3]-N_dwv*dwv,wvs0[2*Npix//3]+N_dwv*dwv,dwv)#-130*dwv/2
+        wvs_max = np.arange(wvs0[-1]-N_dwv*dwv,wvs0[-1]+N_dwv*dwv,dwv)#-125*dwv/2
+        nloglike_arr = np.zeros((np.size(wvs_min),np.size(wvs_mid),np.size(wvs_mid2),np.size(wvs_max)))
+        for k,wv_min in enumerate(wvs_min):
+            for l, wv_mid in enumerate(wvs_mid):
+                for l2, wv_mid2 in enumerate(wvs_mid2):
+                    for m, wv_max in enumerate(wvs_max):
+                        nloglike_arr[k,l,l2,m] = wavcal_nloglike([wv_min,wv_mid,wv_mid2,wv_max,1000,25], x, spectrum, spec_err, star_func, telluric_wvs, telluric_interpgrid, tmp_deg_wvs, blaze_chunks,False,rv)
+        argmin2d = np.unravel_index(np.argmin(nloglike_arr),nloglike_arr.shape)
+
+        spl = InterpolatedUnivariateSpline(x_knots, [wvs_min[argmin2d[0]], wvs_mid[argmin2d[1]], wvs_mid2[argmin2d[2]],
+                                               wvs_max[argmin2d[3]]], k=3, ext=0)
+        wvs00 = spl(x)
+    else:
+        wvs00 = wvs0
+
+    ## Real initialization
+    x_knots = x[np.linspace(0, len(x) - 1, N_nodes_wvs, endpoint=True).astype(np.int)]
+    if fitsrv:
+        paras0 = np.array(wvs00[x_knots].tolist() + [rv,1000,25])
+    else:
+        paras0 = np.array(wvs00[x_knots].tolist() + [1000,25])
+    simplex_init_steps =  np.ones(np.size(paras0))
+    simplex_init_steps[0:N_nodes_wvs] = dwv/4
+    if fitsrv:
+        simplex_init_steps[N_nodes_wvs] = 1
+        simplex_init_steps[N_nodes_wvs+1] = 200
+        simplex_init_steps[N_nodes_wvs+2] = 10
+    else:
+        simplex_init_steps[N_nodes_wvs] = 200
+        simplex_init_steps[N_nodes_wvs+1] = 10
+    initial_simplex = np.concatenate([paras0[None,:],paras0[None,:] + np.diag(simplex_init_steps)],axis=0)
+    res = minimize(lambda paras: wavcal_nloglike(paras, x,spectrum,spec_err,star_func,telluric_wvs,telluric_interpgrid,N_nodes_wvs,blaze_chunks,fitsrv,rv), paras0, method="nelder-mead",
+                           options={"xatol": 1e-10, "maxiter": 1e5,"initial_simplex":initial_simplex,"disp":False})
+    out = res.x
+
+    spl = InterpolatedUnivariateSpline(x_knots, out[0:N_nodes_wvs], k=3, ext=0)
+    return spl(x),out,x_knots
+
+
+def fit_wavecal_fib(init_wvs,combined_spec,combined_err,star_func,star_rv, telluric_wvs, telluric_interpgrid,
+                    N_nodes_wvs=5, blaze_chunks=5,init_grid_search=False,init_grid_dwv=3e-4,mypool = None):
+    """
     Args:
-        mjd: (float) time MJD
-        ra: RA in degrees
-        dec: Dec in degrees
-        star_v: star's radial velocity (km/s)
 
-    Return:
-        rel_v: (float) in km/s
+    Returns:
+        out:
     """
-    sc = SkyCoord(ra=ra*u.deg, dec=dec*u.deg, )
-    barycorr = sc.radial_velocity_correction(obstime=Time(mjd, format='mjd', scale='utc'), location=keck)
-    print(barycorr.to(u.km/u.s).value )
 
-    rel_v = -barycorr.to(u.km/u.s).value + star_v # postiive is redshfit? 
-    
-    return rel_v
+    combined_spec = edges2nans(combined_spec)
+    combined_err = edges2nans(combined_err)
+
+    combined_spec_clip = np.zeros(combined_err.shape)
+    for l in range(combined_spec.shape[0]):
+        combined_spec_clip[l,:] = np.clip(combined_err[l,:], 0.1 * np.nanmedian(combined_err[l,:]), np.inf)
+
+    new_wvs_arr = np.zeros(init_wvs.shape)
+    x = np.arange(0,2048)
+    fitsrv = False
+    if fitsrv:
+        out_paras = np.zeros((combined_spec.shape[0],N_nodes_wvs+3))
+    else:
+        out_paras = np.zeros((combined_spec.shape[0],N_nodes_wvs+2))
+
+    if mypool is None: # jump
+        for l in range(combined_spec.shape[0]):
+        # for l in [6]:
+            spectrum = combined_spec[l,:]
+            spec_err = combined_spec_clip[l,:]
+
+            wvs0 = init_wvs[l,:]
+
+            new_wvs, out, x_knots = _fit_wavecal((x, wvs0, spectrum, spec_err, star_func, telluric_wvs,telluric_interpgrid,
+                                                  N_nodes_wvs, blaze_chunks, fitsrv,star_rv,init_grid_search,init_grid_dwv))
+            new_wvs_arr[l,:] = new_wvs
+            out_paras[l,:] = out
+
+        #     print(out)
+        #     import matplotlib.pyplot as plt
+        #     plt.figure(l+1)
+        #     plt.plot(x, spectrum, label="data")
+        #     plt.fill_between(x, spectrum - spec_err, spectrum + spec_err, label="data err", alpha=0.5)
+        #     plt.plot(x, wavcal_model(out, x, spectrum, spec_err, star_func, telluric_wvs,telluric_interpgrid,
+        #                                   N_nodes_wvs, blaze_chunks, fitsrv, star_rv), label="model")
+        #     plt.legend()
+        # plt.show()
+        # exit()
+    else:
+        outputs_list = mypool.map(_fit_wavecal, zip(itertools.repeat(x),
+                                                    init_wvs, combined_spec, combined_err,
+                                                    itertools.repeat(star_func),
+                                                    itertools.repeat(telluric_wvs),
+                                                    itertools.repeat(telluric_interpgrid),
+                                                    itertools.repeat(N_nodes_wvs),
+                                                    itertools.repeat(blaze_chunks),
+                                                    itertools.repeat(fitsrv),
+                                                    itertools.repeat(star_rv),
+                                                    itertools.repeat(init_grid_search),
+                                                    itertools.repeat(init_grid_dwv)))
+
+        for l, out in enumerate(outputs_list):
+            new_wvs, out, x_knots = out
+            new_wvs_arr[l, :] = new_wvs
+            out_paras[l,:] = out
+
+    model = np.zeros(combined_spec.shape)
+    for l in range(combined_spec.shape[0]):
+        spectrum = combined_spec[l,:]
+        spec_err = copy(combined_spec_clip[l,:])
+        model[l,:] = wavcal_model(out_paras[l,:], x, spectrum, spec_err, star_func, telluric_wvs,telluric_interpgrid,
+                                      N_nodes_wvs, blaze_chunks, fitsrv, star_rv)
+
+    return new_wvs_arr,model,out_paras
