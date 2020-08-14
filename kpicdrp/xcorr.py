73504945a2e055aa0ce653cdf4ff32cced28aacb
Jason Wang <jason.wang@outlook.com>
2020-08-13 17:07:54 -0700

    variable LSF convolution

--- kpicdrp/xcorr.py
+++ kpicdrp/xcorr.py
@@ -3,6 +3,7 @@ import scipy.ndimage as ndi
 import astropy.units as u
 import astropy.constants as consts
 import scipy.optimize as optimize
+import scipy.interpolate as interp
 from PyAstronomy import pyasl
 
 
@@ -103,7 +104,7 @@ def simple_xcorr(shifts, orders_wvs, orders_fluxes, template_wvs, template_fluxe
     return ccf, acf
 
 
-def generate_forward_model_singleorder(fitparams, orders_wvs, star_wvs, star_template_fluxes, template_wvs, template_fluxes, orders_responses, broadened=False):
+def generate_forward_model_singleorder(fitparams, orders_wvs, order_sigmas, star_wvs, star_template_fluxes, template_wvs, template_fluxes, orders_responses, broadened=False):
     rvshift, vsini, pl_flux, star_flux = fitparams
 
 
@@ -116,24 +117,30 @@ def generate_forward_model_singleorder(fitparams, orders_wvs, star_wvs, star_tem
     else:
         broad_model = template_fluxes
 
-    # broaden to instrumental resolution
-    model_r = np.median(template_wvs/np.median(template_wvs - np.roll(template_wvs, 1)))
-    data_r = 35000
-    downsample = model_r/data_r/(2*np.sqrt(2*np.log(2)))
-    broad_model = ndi.gaussian_filter(broad_model, downsample)
+
+
+    thiswvs = orders_wvs
+    star_template = np.interp(thiswvs, star_wvs, star_template_fluxes)
+    star_template /= np.percentile(star_template, 90)
+
 
     new_beta =(rvshift)/consts.c.to(u.km/u.s).value #+  (rel_v)/consts.c.to(u.km/u.s).value
     new_redshift = np.sqrt((1 + new_beta)/(1 - new_beta)) - 1
 
-    thiswvs = orders_wvs
-    thiswvs_starframe = thiswvs/(1+new_redshift)
+    template_wvs_starframe = template_wvs/(1+new_redshift) 
+
+    template = np.interp(template_wvs, template_wvs_starframe, broad_model)
+    template /= np.percentile(template, 90)
+
+    # broaden to instrumental resolution 
+    template = convolve_and_interp(thiswvs, order_sigmas, template_wvs, broad_model)
+    # model_r = np.median(template_wvs/np.median(template_wvs - np.roll(template_wvs, 1)))
+    # data_r = 35000
+    # downsample = model_r/data_r/(2*np.sqrt(2*np.log(2)))
+    # broad_model = ndi.gaussian_filter(broad_model, downsample)
+
     resp_template = orders_responses
 
-    star_template = np.interp(thiswvs, star_wvs, star_template_fluxes)
-    star_template /= np.percentile(star_template, 90)
-
-    template = np.interp(thiswvs_starframe, template_wvs, broad_model)
-    template /= np.percentile(template, 90)
 
     template *= resp_template 
 
@@ -201,14 +208,17 @@ def grid_search(orders_wvs, orders_fluxes, orders_fluxerrs, star_wvs, star_templ
     return loglikes
 
 
-def lsqr_fit(guess, orders, orders_wvs, orders_fluxes, orders_fluxerrs, star_wvs, star_template_fluxes, template_wvs, template_fluxes, orders_responses):
+def lsqr_fit(guess, orders, orders_wvs, orders_sigmas, orders_fluxes, orders_fluxerrs, star_wvs, star_template_fluxes, template_wvs, template_fluxes, orders_responses):
                    
     all_norm_data = []
     all_norm_errs = []
 
     # normalize the data
     for data, errs in zip(orders_fluxes[orders], orders_fluxerrs[orders]):
-        data_continuum = ndi.median_filter(data, 100)
+        bad = np.where(np.isnan(data))
+        data_copy = np.copy(data)
+        data_copy[bad] = np.nanmedian(data)
+        data_continuum = ndi.median_filter(data_copy, 100)
         norm_data = data - data_continuum + np.nanmedian(data_continuum)
         norm_errs = errs #/ data_continuum
 
@@ -228,13 +238,16 @@ def lsqr_fit(guess, orders, orders_wvs, orders_fluxes, orders_fluxerrs, star_wvs
 
             this_fitparams = [shift, vsini, contrast, star_flux]
 
-            model_orders = generate_forward_model_singleorder(this_fitparams, orders_wvs[order], star_wvs, star_template_fluxes, template_wvs, broad_model, orders_responses[order], broadened=True)
-
+            model_orders = generate_forward_model_singleorder(this_fitparams, orders_wvs[order], orders_sigmas[order], star_wvs, star_template_fluxes, template_wvs, broad_model, orders_responses[order], broadened=True)
+            
+            good = np.where(~np.isnan(model_orders))
+            model_orders = model_orders[good]
+            
             model_continuum = ndi.median_filter(model_orders, 100)
             norm_model = model_orders - model_continuum + np.nanmedian(model_continuum)
 
-            norm_data = all_norm_data[i]
-            norm_errs = all_norm_errs[i]
+            norm_data = all_norm_data[i][good]
+            norm_errs = all_norm_errs[i][good]
 
             diff = (norm_model - norm_data)/norm_errs
             all_diffs = np.append(all_diffs, diff[np.where(~np.isnan(diff))])
@@ -252,5 +265,41 @@ def lsqr_fit(guess, orders, orders_wvs, orders_fluxes, orders_fluxerrs, star_wvs
 
     return result
 
+
+def convolve_and_interp(wv_channels, sigmas, model_wvs, model_fluxes, channel_width=None, num_sigma=3):
+    """
+    Simulate the observations of a model. Convolves the model with a variable Gaussian LSF in each spectral channel.
+
+    Args:
+        wv_channels: the wavelengths desired (length of N_output)
+        sigmas: the LSF gaussian stddev of each wv_channels in units of channels (length of N_output)
+        model_wvs: the wavelengths of the model (length of N_model)
+        model_fluxes: the fluxes of the model (length of N_model)
+        channel_width: (optional) the full width of each wavelength channel in units of wavelengths (length of N_output)
+        num_sigma (float): number of +/- sigmas to evaluate the LSF to. 
+
+    Returns:
+        output_model: the fluxes in each of the wavelength channels (length of N_output)
+    """
+    # create the wavelength grid for variable LSF convolution
+    dwv = np.abs(wv_channels - np.roll(wv_channels, 1))
+    dwv[0] = dwv[1] # edge case
+    sigmas_wvs = sigmas * dwv
+    #filter_size_wv = int(np.ceil(np.max(sigmas_wvs))) * 6 # wavelength range to filter
+
+    model_in_range = np.where((model_wvs >= np.min(wv_channels)) & (model_wvs < np.max(wv_channels)))
+    dwv_model = np.abs(model_wvs[model_in_range] - np.roll(model_wvs[model_in_range], 1))
+    dwv_model[0] = dwv_model[1]
+
+    filter_size = int(np.ceil(np.max((2 * num_sigma * sigmas_wvs)/np.min(dwv_model)) ))
+    filter_coords = np.linspace(-num_sigma, num_sigma, filter_size)
+    filter_coords = np.tile(filter_coords, [wv_channels.shape[0], 1]) #  shape of (N_output, filter_size)
+    filter_wv_coords = filter_coords * sigmas_wvs[:,None] + wv_channels[:,None] # model wavelengths we want
+    lsf = np.exp(-filter_coords**2/2)
+
+    model_interp = interp.interp1d(model_wvs, model_fluxes, kind='cubic', bounds_error=False)
+    filter_model = model_interp(filter_wv_coords)
+
+    output_model = np.nansum(filter_model * lsf, axis=1)/np.sum(lsf * lsf, axis=1)
     
-        
+    return output_model
