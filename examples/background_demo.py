e10dd7190608bfa3753546bcab19878b1b12c74c
Jason Wang <jason.wang@outlook.com>
2021-05-23 01:46:34 -0700

    new background API

--- examples/background_demo.py
+++ examples/background_demo.py
@@ -4,23 +4,30 @@
 from glob import glob
 import os
 from astropy.io import fits
-from kpicdrp import background
+import kpicdrp.background as background
+from kpicdrp.data import Dataset, DetectorFrame
 import matplotlib.pyplot as plt
 import numpy as np
 
 # For multiple tint and coadds in one folder
 kpicpublicdir = "fill/in/your/path/public_kpic_data/" # main data dir
-bkgddir = os.path.join(kpicpublicdir,"20200928_backgrounds/") # background frames
-save_loc =os.path.join(bkgddir,"calib","20200928")
+kpicpublicdir = "../public_kpic_data" # main data dir
+bkgddir = os.path.join(kpicpublicdir,"20200928_backgrounds") # background frames
+save_loc =os.path.join(bkgddir,"calib")
 # Create output directory if it does not exist.
 if not os.path.exists(os.path.join(bkgddir,"calib")):
     os.makedirs(os.path.join(bkgddir,"calib"))
+bkgd_fileprefix = "20200928"
+
+# make a list of the input data
 filelist = glob(os.path.join(bkgddir,"raw","*.fits"))
 
+# read in the dataset
+raw_dataset = Dataset(filelist=filelist, dtype=DetectorFrame)
+
 # For multiple tint and coadds in one folder
-# It will save the master backgrounds and bap pixel maps to bkgddir.
-master_bkgds,badpixmaps,\
-smoothed_thermal_noises,unique_tint,unique_coadds = background.process_backgrounds(filelist,save_loc=save_loc)
+# It will save the master backgrounds and bad pixel maps to bkgddir.
+master_bkgds, badpixmaps, unique_tint, unique_coadds = background.process_backgrounds(raw_dataset, save_loc=save_loc, fileprefix=bkgd_fileprefix)
 
 # Plot the resulting master backgrounds and bad pixels
 plt.figure(1)
@@ -28,12 +35,12 @@ for k,(background_med,badpixmap, tint,coadd) in enumerate(zip(master_bkgds,badpi
     print(k)
     plt.subplot(2,len(master_bkgds),k+1)
     plt.title("tint={0} coadd={1}".format(tint,coadd))
-    plt.imshow(background_med, interpolation="nearest", origin="lower")
-    med_val = np.nanmedian(background_med)
+    plt.imshow(background_med.data, interpolation="nearest", origin="lower")
+    med_val = np.nanmedian(background_med.data)
     plt.clim([0,2*med_val])
 
     plt.subplot(2,len(master_bkgds),k+len(master_bkgds)+1)
-    plt.imshow(badpixmap, interpolation="nearest", origin="lower")
+    plt.imshow(badpixmap.data, interpolation="nearest", origin="lower")
 plt.subplot(2,len(master_bkgds),1)
 plt.ylabel("Master backgrounds")
 plt.subplot(2,len(master_bkgds),len(master_bkgds)+1)
@@ -43,6 +50,6 @@ plt.show()
 # OR
 
 # For a single tint/number of coadds
-# master_bkgd, smoothed_thermal_noise, badpixmap = background.make_badpixmap(filelist,plot=True) # does not save automatically
+# master_bkgd, smoothed_thermal_noise, badpixmap = background.create_background_badpixelmap(filelist,plot=True) # does not save automatically
 # background.save_bkgd_badpix(bkgddir,master_bkgd,badpixmap,smoothed_thermal_noise,header=fits.getheader(filelist[0]),
 #                             readnoisebar=False)
--- kpicdrp/data.py
+++ kpicdrp/data.py
@@ -17,7 +17,7 @@ class BasicData():
     Args:
         data (np.array): the actual data
         header: corresponding header
-        filename (str): filepath to a FITS file with data/header information
+        filepath (str): filepath to a FITS file with data/header information  
 
     Fields:
         data (np.array): the actual data
@@ -28,65 +28,110 @@ class BasicData():
     """
     type = "base"
 
-    def __init__(self, data=None, header=None, filename=""):
+    def __init__(self, data=None, header=None, filepath=""):
         if data is not None:
             self.data = data
             self.header = header
-            self.filename = filename
-        elif len(filename) > 0:
+            self.exthdus = None
+        elif len(filepath) > 0:
             # load data/header from disk. assume loaded in primary FITS extention
-            with fits.open(filename) as hdulist:
+            with fits.open(filepath) as hdulist:
                 self.data = hdulist[0].data
                 self.header = hdulist[0].header
-            self.filename = filename
+                # load the extension headers for subclasses to deal with 
+                if len(hdulist) > 1:
+                    self.exthdus = hdulist[1:]
+                else:
+                    self.exthdus = None
         else:
             raise ValueError("Either data or filename needs to be specified. Cannot both be empty")
         
+        # split up filepath into filename and filedir
+        filepath_args = filepath.split(os.path.sep)
+        if len(filepath_args) == 1:
+            # no directory info in filepath, so current working directory
+            self.filedir = "."
+            self.filename = filepath_args[0]
+        else:
+            self.filename = filepath_args[-1]
+            self.filedir = os.path.sep.join(filepath_args[:-1])
 
 
-
-    def save(self, filename=None):
+    def save(self, filename=None, filedir=None):
         """
         Save file to disk with user specified filepath
 
         Args:
             filename (str): filepath to save to. Use self.filename if not specified
+            filedir (str): filedir to save to. Use self.filedir if not specified
         """
         if filename is not None:
             self.filename = filename
+        if filedir is not None:
+            self.filedir = filedir
+        
+        filepath = os.path.join(self.filedir, self.filename)
 
         hdulist = fits.HDUList()
         hdu = fits.PrimaryHDU(data=self.data, header=self.header)
         hdulist.append(hdu)
-        hdulist.writeto(self.filename, overwrite=True)
+        hdulist.writeto(filepath, overwrite=True)
         hdulist.close()
 
 
 
 class Dataset():
     """
-    A sequence of data objects of the same kind. 
+    A sequence of data objects of the same kind. Can be looped over. 
     """
     def __init__(self, data_sequence=None, filelist=None, dtype=None):
         if data_sequence is None and filelist is None:
             raise ValueError("Either data_sequence or filelist needs to be specified")
+
 
         if data_sequence is not None:
             # data is already nicely formatted. No need to do anything
             self.data = data_sequence
             self.type = data_sequence[0].type
         else:
+            if len(filelist) == 0:
+                raise ValueError("Empty filelist passed in")
+    
             # read in data from disk
             if dtype is None:
                 raise ValueError("Need to specify a dtype when passing in a list of files")
             self.data = []
-            for filename in filelist:
-                frame = dtype(filename=filename)
+            for filepath in filelist:
+                frame = dtype(filepath=filepath)
                 self.data.append(frame)
             
             self.type = self.data[0].type
 
-    def save(filedir, filenames=None):
+            # turn lists into np.array for indiexing behavior
+            if isinstance(self.data, list):
+                self.data = np.array(self.data)
+
+    def __iter__(self):
+        self.__count__ = 0
+        return self
+
+    def __next__(self):
+        if self.__count__ < len(self.data):
+            frame = self.data[self.__count__]
+            self.__count__ += 1
+            return frame
+        else:
+            raise StopIteration
+
+    def __getitem__(self, indices):
+        if isinstance(indices, int):
+            # return a single element of the data
+            return self.data[indices]
+        else:
+            # return a subset of the dataset
+            return Dataset(data_sequence=self.data[indices])
+
+    def save(self, filedir=None, filenames=None):
         """
         Save each file of data in this dataset into the indicated file directory
 
@@ -96,9 +141,10 @@ class Dataset():
             filenames = []
             for frame in self.data:
                 filename = frame.filename
-                filename_args = filename.split(os.path.sep)
-                filename = filename_args[-1]
-                filenames.append(filename)
+                filenames.append(frame.filename)
+
+        for filename, frame in zip(filenames, self.data):
+            frame.save(filename=filename, filedir=filedir)
 
         
 class DetectorFrame(BasicData):
@@ -107,8 +153,13 @@ class DetectorFrame(BasicData):
     """
     type = "2d"
 
-    def __init__(self, data=None, header=None, filename=""):
-        super().__init__(data, header, filename)
+    def __init__(self, data=None, header=None, filepath=""):
+        super().__init__(data, header, filepath)
+        
+        if 'ROTATED' not in self.header:
+            # rotate data by -90 degrees to for NIRSPEC "standard"
+            self.data = np.rot90(self.data, -1)
+            self.header['ROTATED'] = True # mark in the header frame has been rotated
 
 
 class Background(DetectorFrame):
@@ -117,11 +168,17 @@ class Background(DetectorFrame):
     """
     type = "bkgd"
 
-    def __init__(self, data=None, header=None, filename=""):
-        super().__init__(data, header, filename)
+    def __init__(self, data=None, header=None, filepath="", data_noise=None):
+        super().__init__(data, header, filepath)
 
+        if data_noise is not None:
+            self.noise = data_noise
+        elif self.exthdus is not None:
+            self.noise = self.exthdus[0].data
+        else: 
+            self.noise = np.zeros(self.data.shape)
 
-    def save(self, filename=None):
+    def save(self, filename=None, filedir=None):
         """
         Save file to disk with user specified filepath
 
@@ -131,19 +188,34 @@ class Background(DetectorFrame):
         self.header['ISCALIB'] = True
         self.header['CALIBTYP'] = "Background"
 
-        super().save(filename)
+        if filename is not None:
+            self.filename = filename
+        if filedir is not None:
+            self.filedir = filedir
+        
+        filepath = os.path.join(self.filedir, self.filename)
+
+        hdulist = fits.HDUList()
+        hdu = fits.PrimaryHDU(data=self.data, header=self.header)
+        hdulist.append(hdu)
+        exthdu1 = fits.ImageHDU(data=self.noise)
+        hdulist.append(exthdu1)
+        hdulist.writeto(filepath, overwrite=True)
+        hdulist.close()
 
 class BadPixelMap(DetectorFrame):
     """
     A badpixelmap frame from the NIRSPEC detector. Has shape 2048x2048
+
+    Bad pixels are set to np.nan. Good pixels are set to 1. 
     """
     type = "badpixmap"
 
-    def __init__(self, data=None, header=None, filename=""):
-        super().__init__(data, header, filename)
+    def __init__(self, data=None, header=None, filepath=""):
+        super().__init__(data, header, filepath)
 
 
-    def save(self, filename=None):
+    def save(self, filename=None, filedir=None):
         """
         Save file to disk with user specified filepath
 
@@ -153,4 +225,16 @@ class BadPixelMap(DetectorFrame):
         self.header['ISCALIB'] = True
         self.header['CALIBTYP'] = "BadPixelMap"
 
-        super().save(filename)
+        super().save(filename=filename, filedir=filedir)
+
+    def mark_bad(self, frame):
+        """
+        Mark bad pixels in input frame as np.nan
+
+        Args:
+            frame (DetectorFrame): frame to mark bad pixels
+        """
+        if not isinstance(frame, DetectorFrame):
+            raise ValueError("input frame needs to be instance of DetectorFrame")
+        
+        frame.data *= self.data # either multiplies by 1 or np.nan
