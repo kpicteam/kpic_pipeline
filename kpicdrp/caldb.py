e719a939eb53329de96a94bb261be11f3a10f006
Taylor Venenciano <tmvb2018@mymail.pomona.edu>
2021-06-08 11:24:19 -1000

    CalDB self.col_names, removed index, made Detector

--- kpicdrp/caldb.py
+++ kpicdrp/caldb.py
@@ -1,25 +1,25 @@
-from kpicdrp.data import BasicData
 import pandas as pd
 import os
 
 # CalDB - most generic class 
 class CalDB():
 
-    column_names = ["Filepath", "Type", "Date/Time of Obs."]
+    def __init__(self, col_names=None, filepath=""):
 
-
-    def __init__(self, filepath=""):
-    
-     # If filepath is specified, read in file and dataframe saved as self.db
-        if len(filepath) > 0: 
-            self.filepath = filepath
-            self.data = pd.read_csv(filepath)
-            self.db = pd.DataFrame(self.data, columns = CalDB.column_names)
-            # only reads in data columns, not index
-        
     # If no filepath is specified, create an empty dataframe with column names
+    # input column_names as "Col1,Col2,Col3" 
+        if col_names is not None:
+            self.columns = col_names.split(',')
+            self.db = pd.DataFrame(columns = self.columns)
+
+        # If filepath is specified, read in file and dataframe is saved as self.db
+        elif len(filepath) > 0: 
+            self.filepath = filepath
+            self.db = pd.read_csv(filepath) 
+            self.columns = list(self.db.columns.values)
+
         else:
-            self.db = pd.DataFrame(columns = CalDB.column_names)
+            print("Filepath and column_names cannot both be blank.") # added error message
 
     # split up filepath into filename and filedir, copied from data.py BasicData class
         filepath_args = filepath.split(os.path.sep)
@@ -32,27 +32,32 @@ class CalDB():
             self.filedir = os.path.sep.join(filepath_args[:-1])
 
 
-    # Adds an entry to bottom of dataframe
-    def add_entry(self, new_entry):
-        # format of new_entry: BasicData(filepath="")
-        self.db = self.db.append(pd.DataFrame([[os.path.join(new_entry.filedir, new_entry.filename), new_entry.type, new_entry.time_obs]], columns = CalDB.column_names), ignore_index = True)
-        
+    # Adds an entry in the dataframe or updates existing
+    def create_entry(self, entry):
+        # format of entry: BasicData(filepath=""), BasicData object
+        # adds 3 values: filepath, type, time of observation
+        if os.path.join(entry.filedir, entry.filename) in self.db.values:
+            row_index = self.db[self.db["Filepath"]==os.path.join(entry.filedir, entry.filename)].index.values
+            self.db.loc[row_index,self.columns] = [os.path.join(entry.filedir, entry.filename), entry.type, entry.time_obs]
+            # replaced existing entry
 
-    # Updates an entry in the dataframe if know index, maintains same index as before
-    def update_entry(self, row_index, updated_entry):
-        # format of updated_entry: BasicData(filepath="")
-        self.db.loc[row_index,CalDB.column_names] = [os.path.join(updated_entry.filedir, updated_entry.filename), updated_entry.type, updated_entry.time_obs]
-
-
-    # Removes an entry in the dataframe if know index, resets index after dropping
-    def remove_entry(self, row_index):
-        # format of row_index: int
-        self.db = self.db.drop([self.db.index[row_index]])
-        self.db = self.db.reset_index(drop=True)
+        else:
+            self.db = self.db.append(pd.DataFrame([[os.path.join(entry.filedir, entry.filename), entry.type, entry.time_obs]], columns = self.columns), ignore_index = True)
+            # added entry to dataframe
 
 
-    # Save dataframeto existing csv file or creates new one based on filepath
-    # saves with index in csv but code opens csv without index
+    # Removes an entry in the dataframe
+    # format of entry: BasicData(filepath=""), BasicData object
+    def remove_entry(self, entry):
+        if os.path.join(entry.filedir, entry.filename) in self.db.values:
+            entry_index = self.db[self.db["Filepath"]==os.path.join(entry.filedir, entry.filename)].index.values
+            self.db = self.db.drop(self.db.index[entry_index])
+            self.db = self.db.reset_index(drop=True)
+        else:
+            return("No filepath found so could not remove.")
+
+
+    # Save dataframe to existing csv file or creates new one based on filepath
     def save(self, filename=None, filedir=None):
         """
         Save file to disk with user specified filepath
@@ -67,14 +72,41 @@ class CalDB():
         if filedir is not None:
             self.filedir = filedir
         
-        self.filepath = os.path.join(self.filedir, self.filename)
+        filepath = os.path.join(self.filedir, self.filename)
 
-        self.db = self.db.reset_index(drop=True) # reset index just in case
-        self.db.to_csv(self.filepath) 
+        self.db.to_csv(filepath, index=False) # doesn't save index
+    # throw an error if filepath is not specified?
+
+
+# subclass for Detector database
+class DetectorCalDB(CalDB):
+
+    def __init__(self, col_names=None, filepath=""):
+        super().__init__(col_names, filepath)
+    # self.columns = define, so don't need to type in
+    # col_names = "Filepath,Type,Date/Time of Obs.,Integration Time,Coadds"
+        if self.columns !=["Filepath", "Type","Date/Time of Obs.", "Integration Time", "Coadds"]:
+            # standard for these names, must type in exact spelling each time?
+            raise ValueError("This is not a Detector Calibration Database. Please use a different type of database.")
+
+    # Adds an entry in the dataframe or updates existing
+    def create_entry(self, entry):
+        # format of entry: BadPixelMap(filepath=""), BadPixelMap object
+        # adds 5 values: filepath, type, time of observation, integration time, coadds
+        if os.path.join(entry.filedir, entry.filename) in self.db.values:
+            row_index = self.db[self.db["Filepath"]==os.path.join(entry.filedir, entry.filename)].index.values
+            self.db.loc[row_index,self.columns] = [os.path.join(entry.filedir, entry.filename), entry.type, entry.time_obs,entry.header["TRUITIME"],entry.header["COADDS"]]
+            # replaced existing entry
+
+        else:
+            self.db = self.db.append(pd.DataFrame([[os.path.join(entry.filedir, entry.filename), entry.type, entry.time_obs,entry.header["TRUITIME"],entry.header["COADDS"]]], columns = self.columns), ignore_index = True)
+            # added entry to dataframe
+
+# save function and remove_entry function is same as CalDB
+# add a function to get best calib file
 
 
 """
 Other sample test code: 
-x.db.loc[index] to access a specific row if know the index as int
 x.db to view database
 """
