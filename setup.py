9e3cbb8e1672664575cc207e277d8c8d5ef8abe0
Jason Wnag <jason.wang@outlook.com>
2021-08-12 14:15:52 -0700

    small setup updates

--- setup.py
+++ setup.py
@@ -4,13 +4,6 @@ import os
 import pathlib
 import configparser
 
-setup(name='kpicdrp',
-      version='1.0',
-      description='KPIC Data Reduction Pipeline',
-      author='KPIC Team',
-      packages=find_packages(),
-     )
-
 ## Create a configuration file for the KPIC DRP if it doesn't exist. 
 
 homedir = pathlib.Path.home()
@@ -18,7 +11,6 @@ config_filepath = os.path.join(homedir, ".kpicdrp")
 if not os.path.exists(config_filepath):
 
       kpicdrp_basedir = os.path.dirname(__file__)
-
       config = configparser.ConfigParser()
       config.read(os.path.join(kpicdrp_basedir, ".kpicdrp_template"))
       config["PATH"]["caldb"] = kpicdrp_basedir
@@ -26,5 +18,14 @@ if not os.path.exists(config_filepath):
             config.write(f)
 
       print("kpicdrp: Configuration file written to {0}. Please edit if you want things stored in different locations.".format(config_filepath))
+
+
+setup(name='kpicdrp',
+      version='1.0',
+      description='KPIC Data Reduction Pipeline',
+      author='KPIC Team',
+      packages=find_packages(),
+     )
+
 
 
